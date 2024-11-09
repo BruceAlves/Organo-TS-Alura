ec6bc1c4c4e8bfaf0ee2167939522613095cbaae
BruceAlves <brucewallace144@exemplo.com>
2024-11-08 22:27:30 -0300

    project organo ts

--- src/componentes/Time/index.tsx
+++ src/componentes/Time/index.tsx
@@ -23,6 +23,7 @@ const Time = (props: TimesProps) => {
                         nome={colaborador.nome} 
                         cargo={colaborador.cargo} 
                         imagem={colaborador.imagem}
+                        data={colaborador.data}
                      /> )}
             </div>
         </section> 
